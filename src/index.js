bd461f935c451ec436b339cff781fe4d0609602d
piyush singh <piyush110804@gmail.com>
2026-07-22 11:03:56 +0530

    setup a dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
- 
+const express=require("express");
+const {PORT}=require('./config/serverconfig');
+
+ const SetupAndStartServer= async ()=>{
+  const app=express();
+  app.listen(PORT,()=>{
+    console.log(`server started at port ${PORT}`);
+  })
+
+ }
+ SetupAndStartServer();
