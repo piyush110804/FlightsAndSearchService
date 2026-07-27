73ffce1a2fce315566fe3a430595719d5a703e3c
piyush singh <piyush110804@gmail.com>
2026-07-27 11:01:37 +0530

    Added city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,20 +1,61 @@
-const { where } = require('sequelize');
+
 const {City}=require('../models/index');
 
-class cityrepository{
+
+class Cityrepository{
  async createcity({name}){
    try{
     const city= await City.create({name});
     return city;
-   }
+   }  
    catch(error){
-    throw error;
+    console.log('Something went wrong in repository layer');
+      throw {error};
    }
 
    
  }
+ async deletecity(cityid){
+      try{
+         await City.destroy({
+          where:{
+            id:cityid
+          }
+         })
+         return true;
+      }
+      catch(error){
+       console.log('Something went wrong in repository layer');
+      throw {error};
+      }
+      
+ }
+ async updatecity(data,cityid){
+    try{
+       const city=await City.update(data,{
+        where:{
+          id:cityid
+        }
+       })
+       return city;
+    }
+    catch(error){
+      console.log('Something went wrong in repository layer');
+      throw {error};
+    }
+ }
+ async getcity(cityid){
+  try{
+    const city=await City.findByPk(cityid);
+    return city;
+  }
+  catch(error){
+    console.log('Something went wrong in repository layer');
+      throw {error};
+  }
+ }
  
 
 }
-module.exports=cityrepository;
+module.exports=Cityrepository;
 
