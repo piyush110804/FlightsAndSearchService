87f6f587c68cfa2c68241bad9811e73bd84dd55e
piyush singh <piyush110804@gmail.com>
2026-08-30 21:27:27 +0530

    added flight create request middleware

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const router=express.Router();
+const {FlightMiddlewares}=require('../../middlewares/index');
 const CityController=require('../../controllers/city-controller');
 const FlightController=require('../../controllers/flight-controller');
 const AirportController=require('../../controllers/airport-controller');
@@ -8,9 +9,15 @@ router.delete('/city/:id',CityController.destroy);
 router.patch('/city/:id',CityController.update);
 router.get('/city/:id',CityController.get);
 router.get('/city',CityController.getAll);
-router.post('/flights',FlightController.create);
+router.post(
+    '/flights',
+    FlightMiddlewares.validateCreateFlight,
+    FlightController.create
+);
+
 router.get('/flights',FlightController.getAll);
 router.post('/airports',AirportController.create);
+
 module.exports=router;
 
 
